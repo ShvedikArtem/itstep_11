73287fcf24b59a54f28686c50e0772314b49aaf5
ShvedikArtem <gameknightytpm@gmail.com>
2024-12-08 14:39:33 +0200

    Add files via upload

---
+++ Яка у тебе оцінка.py
@@ -0,0 +1,9 @@
+grade = int(input("Введіть чесно свою оцінку: "))
+if grade >=0 and grade<=49:
+    print("Незадовільно")
+elif grade >=50 and grade<=69:
+    print("Задовільно")
+elif grade >=70 and grade<=89:
+    print("Добре")
+elif grade >= 90 and grade <= 100:
+    print("Відмінно")
