b1287380be513d3e772adc4313c220484ce9b8b1
ShvedikArtem <gameknightytpm@gmail.com>
2024-12-08 14:35:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-grade = int(input("Введіть чесно свою оцінку: "))
-if grade >=0 and grade<=49:
-    print("Незадовільно")
-elif grade >=50 and grade<=69:
-    print("Задовільно")
-elif grade >=70 and grade<=89:
-    print("Добре")
-elif grade >= 90 and grade <= 100:
-    print("Відмінно")
+User = int(input("Введіть скільки вам років: "))
+if User >= 18:
+    print("Доступ дозволено")
+elif User < 18:
+    print("У доступі відмовлено")
