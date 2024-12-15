4cebac08794df57167e1f18c290aba7d8c589232
ShvedikArtem <gameknightytpm@gmail.com>
2024-12-15 12:41:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-User = int(input("Введіть скільки вам років: "))
-if User >= 18:
-    print("Доступ дозволено")
-elif User < 18:
-    print("У доступі відмовлено")
+class Human:
+    def __init__(self, name = "Human"):
+        self.name = name
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+
+    def add_passenger(self, *args):
+        for passenger in args:
+            self.passengers.append(passenger)
+
+    def print_passengers_names(self):
+        if self.passengers != []:
+            print(f"Names of {self.brand} passengers")
+            for passenger in self.passengers:
+                print(passenger.name)
+        else:
+            print(f"There are no passengees in {self.brand}")
+
+nick = Human("Nick")
+kate = Human("Kate")
+car = Auto("Mercedes")
+car.add_passenger(nick)
+car.add_passenger(kate)
+car.print_passengers_names()
+
